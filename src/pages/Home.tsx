7d2efd21c4d7a6f3c07d9fcc5ffa395df1e53ba3
Erik Qodeless <git.qodeless@outlook.com>
2021-11-27 17:02:02 -0300

    Novos botões

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,51 @@
 import '../Home.css';
-import { Box, Button } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCloud } from '@fortawesome/free-solid-svg-icons';
-import { fontSize } from '@material-ui/system';
 
 export default function Home(){
     return (
       <div className="Home">
         <header className="Home-header">
-          <button type="submit" className="botao-entrar">ENTRAR</button>
-          <button type="submit" className="botao-cadastrar">CADASTRAR</button>
           <div className="weatherLabelIcon">  
             <FontAwesomeIcon style={{color:'white', fontSize: 35, paddingRight:'10px'}} icon={faCloud}/>
             <h3 className="weather-label">Weather</h3>
+          </div>  
+          <div>
+            <Button 
+              type="submit" 
+              style={{
+                backgroundColor: 'white', 
+                fontWeight: 'bold', 
+                color: '#5EA8EC',
+              }} 
+              sx={{
+                borderRadius:'30px', 
+                width:'115px', 
+                height: '35px', 
+                top: '15px',
+                right: '50px'
+              }}
+            >
+              CADASTRAR
+            </Button>
+            <Button 
+              type="submit" 
+              style={{
+                backgroundColor:'#5EA8EC', 
+                fontWeight:'bold', 
+                color: 'white'
+              }} 
+              sx={{
+                borderRadius:'30px', 
+                width:'90px', 
+                height: '35px', 
+                top: '15px',
+                right: '25px'
+              }}
+            >
+              ENTRAR
+            </Button>
           </div>  
         </header>
         <body className="Home-body">
